714fd8413c6d0ce207192cd688a00b5c8d1867bf
Cacolar <cacolar.work@gmail.com>
2025-06-26 16:11:03 +0800

    Changes to be committed: 	modified:   icon.png 	modified:   index.html 	modified:   manifest.json 	modified:   script.js 	new file:   script.min.js

--- script.js
+++ script.js
@@ -2,71 +2,85 @@ const publicVapidKey = 'BJ5lPY0qjF1Tx9v9AvS7ajodgmXdmOCiPwpROPmBMY2Jk3DRaxCe6q8N
 const host = 'https://stage.usdt.town'
 const uid = generateUUID();
 const dom = {
-    loading: document.querySelector('.spinner-border'),
-    btn: document.querySelector('.btn-go'),
+	loading: document.querySelector('.spinner-border'),
+	main: document.querySelector('.d-main'),
+	btn: document.querySelector('.btn-go'),
 }
 const options = {
-    userVisibleOnly: true,
-    applicationServerKey: urlB64ToUint8Array(publicVapidKey)
+	userVisibleOnly: true,
+	applicationServerKey: urlB64ToUint8Array(publicVapidKey)
 };
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-        .then(reg => {
-            console.log('Service Worker successful registration:', reg);
-            dom.loading.classList.add('d-none')
-            dom.btn.classList.remove('d-none')
-        })
-        .catch(err => {
-            alert('Service Worker registration failed:' + err)
-            console.error('Service Worker registration failed:', err)
-        });
+async function registerServiceWorker() {
+	try {
+		const reg = await navigator.serviceWorker.register('./sw.js');
+		console.log('Service Worker successful registration:', reg);
+		dom.loading.classList.add('d-none');
+		dom.main.classList.remove('d-none');
+	} catch (err) {
+		handleError('Service Worker registration failed:', err);
+	}
 }
 
-dom.btn.onclick = async _ => {
-    dom.btn.disabled = true
-    dom.loading.classList.remove('d-none')
-    dom.btn.classList.add('d-none')
-    try {
-        const permission = await Notification.requestPermission()
-        if (permission !== 'granted') {
-            alert('Notification.requestPermission error:'+permission)
-            return
-            window.location.href = `${host}/vapid/${uid}`
-        }
-        const reg = await navigator.serviceWorker.ready
-        reg.pushManager.subscribe(options)
-            .then(async subscription => {
-                console.log('User Subscription:', subscription);
-                const form = new FormData()
-                form.append('json', JSON.stringify(subscription))
-                form.append('uid', uid)
-                await fetch(host + '/vapid', {
-                    method: 'POST',
-                    body: form
-                })
-                window.location.href = `${host}/vapid/${uid}`
-            })
-    } catch (error) {
-        console.error('Subscription failed:', error);
-        alert('Subscription failed:'+ error);
-        return
-        window.location.href = `${host}/vapid/${uid}`
-    }
-    
+async function subscribeUser() {
+	dom.btn.disabled = true
+	dom.loading.classList.remove('d-none')
+	dom.main.classList.add('d-none')
+
+	try {
+		const permission = await Notification.requestPermission()
+		if (permission !== 'granted') {
+			redirect()
+			return
+		}
+
+		const reg = await navigator.serviceWorker.ready
+		const subscription = await reg.pushManager.subscribe(options)
+		console.log('User Subscription:', subscription);
+
+		const form = new FormData()
+		form.append('json', JSON.stringify(subscription))
+		form.append('uid', uid)
+		await fetch(host + '/vapid', {
+			method: 'POST',
+			body: form
+		})
+
+		redirect()
+	} catch (err) {
+		handleError('Subscription failed:', err);
+	}
+}
+
+function handleError(message, err) {
+	console.error(message, err);
+	redirect();
+}
+
+function redirect() {
+	dom.loading.classList.add('d-none');
+	dom.main.classList.remove('d-none');
+	dom.btn.disabled = false;
+	window.location.href = `${host}/vapid/${uid}`;
 }
 
 function urlB64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-    const rawData = window.atob(base64);
-    return new Uint8Array([...rawData].map(char => char.charCodeAt(0)));
+	const padding = '='.repeat((4 - base64String.length % 4) % 4);
+	const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+	const rawData = window.atob(base64);
+	return new Uint8Array([...rawData].map(char => char.charCodeAt(0)));
 }
 
 function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        const r = Math.random() * 16 | 0;
-        const v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		const r = Math.random() * 16 | 0;
+		const v = c === 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
 }
+
+if ('serviceWorker' in navigator) {
+	registerServiceWorker()
+}
+
+dom.btn.onclick = subscribeUser;
