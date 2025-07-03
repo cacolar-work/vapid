f8f3f3f23a19791b384bbd6485705c88ffb74423
Cacolar <cacolar.work@gmail.com>
2025-07-03 18:02:34 +0800

    update script

--- script.js
+++ script.js
@@ -83,7 +83,6 @@ async function checkSubscriptionAndRedirect() {
     try {
         const reg = await navigator.serviceWorker.ready;
         const subscription = await reg.pushManager.getSubscription();
-		console.log('Current Subscription:', subscription);
         if (subscription == null) {
 			dom.loading.classList.add('d-none');
 			dom.main.classList.remove('d-none');
